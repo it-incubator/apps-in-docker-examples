44930152a7c9878e1feb77e7b33a05a3d49cdab6
kuzyuberdin <kuzyuberdin@gmail.com>
2024-10-01 20:00:46 +0400

    remove host

--- 01-nodejs-server/src/index.js
+++ 01-nodejs-server/src/index.js
@@ -1,27 +1,26 @@
 // server.js
 const http = require('http');
 
-const hostname = '0.0.0.0';
-
 let counter = 0;
 
 const server = http.createServer((req, res) => {
     console.log('request: ' + (++counter))
+
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     if (req.url === '/hello') {
         res.statusCode = 200;
-        res.end('Привет от it-incubator.io сервера!\n');
+        res.end('Привет от it-incubator.io сервера!');
     } else if (req.url === '/error') {
         throw new Error('Fatal error, samurai')
     } else {
         res.statusCode = 404;
-        res.end('Страница не найдена\n');
+        res.end('Страница не найдена');
     }
 });
 
 const port = 3000;
-server.listen(port, hostname, () => {
-    console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Сервер запущен по адресу ${port}/`);
 });
